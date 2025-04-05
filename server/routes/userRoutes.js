76049c9db7e1f4f23c6ad20bc43986b3532c5791
Bablu Mehta <babloomehta895012@gmail.com>
2025-04-05 13:47:21 +0530

    login route added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -10,6 +10,11 @@ router.get("/", async (req, res) => {
   res.json({ message: "Hello world" });
 });
 
+router.get("/allUsers", async (req, res) => {
+  let users = await User.find({});
+  res.status(200).json({ users });
+});
+
 router.post("/register", async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -19,11 +24,11 @@ router.post("/register", async (req, res) => {
     if (user) {
       return res.status(400).json({ message: "User already exist" });
     }
-    
+
     //Hash the password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-    
+
     //create new user
     user = new User({ email, password: hashedPassword });
     await user.save();
@@ -36,6 +41,35 @@ router.post("/register", async (req, res) => {
     res.status(201).json({ message: "User registered successfully", token });
   } catch (error) {
     res.status(500).json({ message: "Server error", error });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    
+    if (!user) {
+      return res.status(400).json({ message: "Invalide Credentials" });
+    }
+
+    //compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalide Credentials" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, "secret_key", {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({
+      message: "Login Successfully",
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Server erro", error });
   }
 });
 
